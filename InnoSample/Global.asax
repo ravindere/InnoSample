4bed69f59481b0f087199a1ce9f814c71ce763e2
ravindere <ravinder.edunuri@gmail.com>
2017-11-08 02:18:01 -0700

    Add project files.

---
+++ InnoSample/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="InnoSample.MvcApplication" Language="C#" %>
---
+++ InnoSample/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(InnoSample.Startup))]
+namespace InnoSample
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
